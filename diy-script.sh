9d2287ac5802b389327744ff07fac51c2484d3c4
haixing1001 <50311834+haixing1001@users.noreply.github.com>
2024-05-30 01:55:50 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -2,3 +2,4 @@
 
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+rm .config
